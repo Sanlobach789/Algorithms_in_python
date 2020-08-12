1cd49a6b5210b838f0cbcf9739494cbd55f3e0eb
sanlobach <sanlobach789@gmail.com>
2020-08-12 09:54:52 +0300

    last commit

--- les_1/task_1.py
+++ les_1/task_1.py
@@ -1,4 +1,5 @@
 #Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+# https://drive.google.com/file/d/1juf_LBYRMuwNtwc7idRXalmPOXkdDQs6/view?usp=sharing
 
 x = int(input("Введите трехзначное число х: "))
 a = x // 100
